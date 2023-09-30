9e4e0e30b55148be9940a1fd95e8146530602b46
Hung071224 <hung4ngonvt@gmail.com>
2023-09-30 21:22:08 +0700

    first commit

---
+++ Python New/Day1.py
@@ -0,0 +1,215 @@
+#1
+"""
+
+print("hello Baby :)))")
+
+"""
+
+#2
+
+
+"""
+name =  "Hung"
+money = 100000
+GPA = 3.0
+#2 cai 1 dong
+print(GPA,money)
+
+
+
+print(name, end ="")
+print(money)
+#f-string 
+
+"""
+
+# input: sung dung ham input()
+"""
+name = input("Input full name:")
+
+print(f"welcome, {name}!!!!")
+
+
+a = input("Input number a:")
+b = input("Input number b:")
+
+print(f"sum {a} + sum {b} ={a+b}")
+"""
+#(""" ko chay)
+
+
+"""
+# kieu du lieu
+##so nguyen(int)
+###so thuc(float)
+####logic/boolean (bool)
+#####xau ki tu (string)
+#-.....
+"""
+
+
+"""
+a= int(input("Input number a: "))
+b= int(input("Input number b: "))
+
+print(f"sum {a} + sum {b} ={a+b}")
+
+"""
+
+
+
+"""
+a= float(input("Input number a: "))
+b= float(input("Input number b: "))
+
+print(f"sum {a} + sum {b} ={a+b}")
+
+"""
+
+"""
+# loi !!!
+
+a= int(input("Input number a: "))
+b= input("Input number b: ")
+
+print(f"sum {a} + sum {b} ={a+b}")
+
+"""
+
+
+"""
+# nhan ban (nhan)
+a= int(input("Input number a: "))
+b= input("Input number b: ")
+
+print(f"sum {a} + {b} ={a*b}")
+
+#(+ hoac * deu dc)
+"""
+
+
+
+"""
+
+print(30 * "+")
+
+"""
+
+#bien dich___thong dich 
+
+#(python la thong dich)
+"""
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+
+
+a= "vip pro"
+
+print (a + 2)
+
+print ("hello world")
+
+
+"""
+
+"""
+#(tinh + - * / ....)
+
+
+print(17%2)
+
+"""
+
+
+"""
+#(so sanh)
+print(3==5)
+
+
+print(3>5
+
+"""
+
+
+
+
+"""
+and: phep va
+or: phêp hoac
+not: phep phu dinh
+
+
+(toan hang 1)   and/or/not  (toan hang 2 )
+"""
+
+"""
+a=5
+b=5
+
+           
+print((a < b) or (a >= b))
+            TRUE
+
+
+print((a > b) or (a < b))
+           FALSE
+           
+
+rint(not (a > b))
+        false
+
+
+"""
+
+
+"""
+#----------------------------------------------------------------#
+
+
+
+#1.
+money= int(input("Input number a: "))
+hour= float(input("Input number b: "))
+
+print(f"sum {money} * {hour} ={money*hour}")
+
+salary = money * hour
+print(f"bạn làm được {int(salary)} VNĐ tiền lương!")
+
+#----------------------------------------------------------------#
+
+"""
+
+
+
+
+
+"""
+
+#----------------------------------------------------------------#
+
+
+#2.
+g= float(input("Input number g: "))
+h= int(input("Input number h: "))
+
+#(g = 9.8) cách 2
+
+v = (2 * g * h)**0.5
+
+#(v = (2 * 9.8 * h)**0.5) cách 2
+
+print(f"kết quả là {v=} m/s")
+
+print(f"kết quả là {round(v=,2)} m/s")
+
+
+#----------------------------------------------------------------#
+
+
+"""
+
+
+
