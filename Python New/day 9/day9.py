9e392e579b69fc7d3e82fe70d8348009f84b64bb
Hung071224 <hung4ngonvt@gmail.com>
2023-12-02 19:48:34 +0700

    new

---
+++ Python New/day 9/day9.py
@@ -0,0 +1,154 @@
+
+
+
+
+#hàm cộng hai số
+"""
+
+def tong2so(a, b):
+    tong = a + b
+    return tong
+print(tong2so(5, 10))
+
+"""
+
+
+
+
+
+
+#nhập các hệ số a, b,c 
+"""
+def nhap_đu_lieu():
+    a = float(input("cho biết hệ số a: "))
+    b = float(input("cho biết hệ số b: "))
+    c = float(input("cho biết hệ số c: "))
+    return a, b, c
+"""
+
+
+#hàm thực hiện 4 phép toán
+"""
+
+def pheptoan(a, b):
+    tong = a + b
+    hieu = a - b
+    tich = a * b
+    thuong = a / b
+    return tong, hieu, tich, thuong
+    
+"""
+
+
+#phương trình có 2 nghiệm phân biệt
+"""ư
+
+def phuong_trinh_hai_nghiem(a, b, delta):
+    x1 = (-b + sqrt(delta)) / (2 * a) #tính x1
+    x2 = (-b - sqrt(delta)) / (2 * a) #tính x2
+    print (f"phương trình có hai nghiệm: ")
+    print (f"x1 = {1: .2f}, x2 = {x2: .2f}")
+    
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
