5ecb36809ffa2c1728f0f90889552fd1d537c528
Hung071224 <hung4ngonvt@gmail.com>
2023-12-09 21:13:30 +0700

    new

--- Python New/day10/day10.py
+++ Python New/day10/day10.py
@@ -10,25 +10,50 @@ def fibonacci(n):
 n = int(input("enter a positive integer: "))
 print(f"The number of pairs of rabbit in {n} months is {fibonacci(n)}")
 
+
 """
 
 
 
+#cách 1: need help 
+
+a = int(input("Nhập tử số: "))
+b = int(input("Nhập mẫu số: "))
+u = math.gcd(a, b)
+p = a // u
+q = b // u 
+print ("phân số rút gọn là: ", p, "/", q)
 
 
 
 
 
 
+#cách 2
 
-
-
-
-
-
-
-
-
+a = int(input("Nhập tử số: "))
+b = int(input("Nhập mẫu số: "))
+u = a
+v = b
+q = a%v
+r = b%v
+while u !=0 or v !=0:
+    if u == 0:
+        print("Phân số rút gọn là: ", q, "/", r)
+        break
+    elif v == 0 :
+        print("Phân số rút gọn là: ", q, "/", r)
+        break
+    if q % r !=0:
+        print("Phân số rút gọn là: ", q, "/", r)
+        if u >v:
+            u -= q*r
+        else:
+            v-= q*r
+    q= a % v
+    r= b % v
+if u !=0 or v !=0:
+    print("Phân số rút gọn là: ", q, "/", r)
 
 
 
