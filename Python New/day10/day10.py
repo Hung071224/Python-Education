ec474d8e0b825c3acdafda1314f504fc03979765
Hung071224 <hung4ngonvt@gmail.com>
2023-12-09 20:19:32 +0700

    new

--- Python New/day10/day10.py
+++ Python New/day10/day10.py
@@ -8,7 +8,7 @@ def fibonacci(n):
     else:
         return fibonacci(n-1) + fibonacci(n-2)
 n = int(input("enter a positive integer: "))
-print(f"The number of pairsa of rabbit in {n} months is {fabonacci(n)}")
+print(f"The number of pairs of rabbit in {n} months is {fibonacci(n)}")
 
 
 
