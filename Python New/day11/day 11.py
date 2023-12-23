59f37f2efdc0577c84835b2e1e38416518ef5d77
Hung071224 <hung4ngonvt@gmail.com>
2023-12-23 19:40:19 +0700

    new

--- Python New/day11/day 11.py
+++ Python New/day11/day 11.py
@@ -2,21 +2,13 @@
 
 
 
+
+
+
 fh = open("C:\\Users\\PC\\Python Education\\Python New\\day11\\test.txt" , "r")
-
-
-"""
 print (fh)
-
-"""
-
-"""
 for line in fh:
     print(line)
-
-"""
-
-
 lst = []
 for line in fh:
     line = line.replace("\n", "")
