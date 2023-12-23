b0bf210edd043705ee2fbd2aa086695829f12527
Hung071224 <hung4ngonvt@gmail.com>
2023-12-23 20:22:52 +0700

    new

--- Python New/day11/day 11.py
+++ Python New/day11/day 11.py
@@ -37,7 +37,7 @@ lst
 
 
 
-
+"""
 
 with open("C:\\Users\\PC\\Python Education\\Python New\\day11\\vanban.txt", "r") as f:
     data = f.read()
@@ -45,12 +45,23 @@ with open("C:\\Users\\PC\\Python Education\\Python New\\day11\\vanban.txt", "r")
     num_words = len(words)
     print("Số từ trong file là: ", num_words)
     
+"""
 
 
 
 
-
-
+with open("C:\\Users\\PC\\Python Education\\Python New\\day11\\dayso.txxt", "r") as f:
+    count ={}
+    for line in f:
+        numbers = [int(x) for x in line.split()]
+        for num in numbers:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] =1
+    print("Có", len(count), "Số Nguyên khác nhau trong file")
+    for num, freq in count.items():
+        print("Số", num,"xuất hiện", freq, "lần") 
 
 
 
