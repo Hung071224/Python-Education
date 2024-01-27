3e0a02ffa67a79b37c31d643f115e134ec357e44
Hung071224 <hung4ngonvt@gmail.com>
2024-01-27 20:29:15 +0700

    new

---
+++ Python 2/day1/day1.py
@@ -0,0 +1,95 @@
+
+
+
+"""
+class counter:
+    def __init__(self, count):
+        self.count = count
+    
+    def __tick__(self):
+        self.count += 1
+    
+    def __reset__(self): 
+        self.count = 0
+    
+obj = counter(0)
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+#BAI 2  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
