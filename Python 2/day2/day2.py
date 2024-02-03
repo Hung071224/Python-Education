b91315bbbd3eafc91c0d45579c680a65aa5c4568
Hung071224 <hung4ngonvt@gmail.com>
2024-02-03 20:45:30 +0700

    e

--- Python 2/day2/day2.py
+++ Python 2/day2/day2.py
@@ -6,7 +6,6 @@
 
 
 
-
 class DragonBall:
     def __init__(self, name, level, hair, power):
         self.name = name
@@ -17,35 +16,31 @@ class DragonBall:
     def level_up(self):
         self.level += 1
         self.power += 1000
-        print(f"{self.name} đã tăng lên level {self.level} và có power là {self.power}")
+        print(f"Level Up!!!!!")
     
     def reset(self):
         self.level = 1
         self.power = 10
         self.hair = "black"
-        print(f"{self.name} đã trở lại level ban đầu và có power là {self.power}")
+        print(f"Reset!!!")
         
     def super_saiyan(self, number, hair_color):
         self.level += (number * 10)
         self.hair = hair_color
         self.power *= number
-        print(f"{self.name} đã biến thành super saiyan {number} với màu tóc  là {self.hair} và có level là {self.level}")
+        print(f"SUPER SAIYAN!!!!!")
     
     def show(self):
-        print(f"Thông tin của nhân vật {self.name}: ")
-        print(f"Level: {self.level} ")
-        print(f"Hair: {self.hair} ")
-        print(f"Powers: {self.power} ")
+        print(f"Name: {self.name} ,Level: {self.level},Hair: {self.hair},Powers: {self.power}")
+
                       
+
+
+
 chap1 = DragonBall("Goku", 1, "black", 1000)
 chap1.show()
 chap1.level_up()
 chap1.show()
-chap1.super_saiyan("yellow")
-chap1.show()
-
-
-
 
 
 
